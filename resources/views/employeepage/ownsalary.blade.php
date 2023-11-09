6458b418036aa6bc015c54c08bb3fe4f1a42308e
dynellayon <danielayon2@gmail.com>
2023-11-09 15:38:08 +0800

    second commit

---
+++ resources/views/employeepage/ownsalary.blade.php
@@ -0,0 +1,168 @@
+
+@extends('layouts.master')
+@section('content')
+    {{-- message --}}
+    {!! Toastr::message() !!}
+
+    <!-- Page Wrapper -->
+    <div class="page-wrapper">
+        <!-- Page Content -->
+        <div class="content container-fluid">
+            <!-- Page Header -->
+            <div class="page-header">
+                <div class="row align-items-center">
+                    <div class="col">
+                        <h3 class="page-title">Employee Salary <span id="year"></span></h3>
+                        <ul class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Salary</li>
+                        </ul>
+                    </div>
+                    <div class="col-auto float-right ml-auto">
+                        <a href="#" class="btn add-btn" data-toggle="modal" data-target="#add_salary"><i class="fa fa-plus"></i> Add Salary</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Search Filter -->
+            <div class="row filter-row">
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-12">  
+                    <div class="form-group form-focus">
+                        <input type="text" class="form-control floating">
+                        <label class="focus-label">Employee Name</label>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-12">  
+                    <div class="form-group form-focus select-focus">
+                        <select class="select floating"> 
+                            <option value=""> -- Select -- </option>
+                            <option value="">Employee</option>
+                            <option value="1">Manager</option>
+                        </select>
+                        <label class="focus-label">Role</label>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-12"> 
+                    <div class="form-group form-focus select-focus">
+                        <select class="select floating"> 
+                            <option> -- Select -- </option>
+                            <option> Pending </option>
+                            <option> Approved </option>
+                            <option> Rejected </option>
+                        </select>
+                        <label class="focus-label">Leave Status</label>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-12">  
+                    <div class="form-group form-focus">
+                        <div class="cal-icon">
+                            <input class="form-control floating datetimepicker" type="text">
+                        </div>
+                        <label class="focus-label">From</label>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-12">  
+                    <div class="form-group form-focus">
+                        <div class="cal-icon">
+                            <input class="form-control floating datetimepicker" type="text">
+                        </div>
+                        <label class="focus-label">To</label>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 col-xl-2 col-12">  
+                    <a href="#" class="btn btn-success btn-block"> Search </a>  
+                </div>     
+            </div>
+            <!-- /Search Filter -->  
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="table-responsive">
+                        <table class="table table-striped custom-table datatable">
+                            <thead>
+                                <tr>
+                                
+                                    <th>Employee</th>
+                                    <th>Email</th>
+                                    <th>Join Date</th>
+                                    <th>Role</th>
+                                    <th>Salary</th>
+                                    <th>Payslip</th>
+                                    
+                                
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($ownsalary as $items)
+                                <tr>
+                                    <td>
+                                        <h2 class="table-avatar">
+                                            
+                                            <a >{{ $items->name }}</a>
+                                        </h2>
+                                    </td>
+                                    <td>{{ $items->email }}</td>
+                                    <td>{{ $items->join_date }}</td>
+                                    <td>{{ $items->role_name }}</td>
+                                    <td>₱{{ $items->salary }}</td>
+                                    <td hidden class="salary">{{ $items->salary }}</td>
+                                    <td><a class="btn btn-sm btn-primary" href="{{ url('form/salary/view/'.$items->user_id) }}" target="_blank">Generate Slip</a></td>
+                                    
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /Page Content -->
+
+</div>
+    @section('script')
+        <script>
+            $(document).ready(function() {
+                $('.select2s-hidden-accessible').select2({
+                    closeOnSelect: false
+                });
+            });
+        </script>
+        <script>
+            // select auto id and email
+            $('#name').on('change',function()
+            {
+                $('#employee_id').val($(this).find(':selected').data('employee_id'));
+            });
+        </script>
+        {{-- update js --}}
+        <script>
+            $(document).on('click','.userSalary',function()
+            {
+                var _this = $(this).parents('tr');
+                $('#e_id').val(_this.find('.id').text());
+                $('#e_name').val(_this.find('.name').text());
+                $('#e_salary').val(_this.find('.salary').text());
+                $('#e_basic').val(_this.find('.basic').text());
+                $('#e_da').val(_this.find('.da').text());
+                $('#e_hra').val(_this.find('.hra').text());
+                $('#e_conveyance').val(_this.find('.conveyance').text());
+                $('#e_allowance').val(_this.find('.allowance').text());
+                $('#e_medical_allowance').val(_this.find('.medical_allowance').text());
+                $('#e_tds').val(_this.find('.tds').text());
+                $('#e_esi').val(_this.find('.esi').text());
+                $('#e_pf').val(_this.find('.pf').text());
+                $('#e_leave').val(_this.find('.leave').text());
+                $('#e_prof_tax').val(_this.find('.prof_tax').text());
+                $('#e_labour_welfare').val(_this.find('.labour_welfare').text());
+            });
+        </script>
+         {{-- delete js --}}
+    <script>
+        $(document).on('click','.salaryDelete',function()
+        {
+            var _this = $(this).parents('tr');
+            $('.e_id').val(_this.find('.id').text());
+        });
+    </script>
+    @endsection
+@endsection
